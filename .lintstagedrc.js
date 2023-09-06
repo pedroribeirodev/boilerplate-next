02e498fb6c4cbfdb93a59956e443f14a58b617b9
Pedro Ribeiro <pedro.bonfim.13@gmail.com>
2023-09-06 19:35:15 -0300

    Initial jest config

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --fike')}`
+    `npm run lint --fix . ${filenames.join(' --fike')}`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
